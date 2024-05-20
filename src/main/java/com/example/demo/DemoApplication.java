5af19e4c1c45c458eb1cc29919f5283269d00031
KB <kokbeng.ong@prudential.com.my>
2024-05-20 17:26:49 +0800

    testing

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -1,22 +1,22 @@
 package com.example.demo;
 
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
 
 @SpringBootApplication
 public class DemoApplication {
 
+	private static final Logger logger = LoggerFactory.getLogger(DemoApplication.class);
+
 	public void demoFunc1(){
-		int total=0;
-		for(int i=1;i<=10;i++){
-			total+=i;
-		}
+		logger.info("demoFunc1...");
 	}
 	public void demoFunc2(){
-		int total=0;
-		for(int i=1;i<=10;i++){
-			total+=i;
-		}
+		logger.info("demoFunc2...");
 	}
 
 	public static void main(String[] args) {
